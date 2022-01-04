8e4db36df5284b51dc725e63bf972c99aa2b157f
akmalsyrf <dev.akmal69@gmail.com>
2022-01-04 13:22:29 +0700

    update

--- client/src/pages/Complain.js
+++ client/src/pages/Complain.js
@@ -1,79 +1,126 @@
-// import hook
-import React, { useState, useEffect } from 'react'
+// import useContext hook
+import React, { useState, useEffect, useContext } from "react";
 
-import Navbar from '../components/Navbar'
+import Navbar from "../components/Navbar";
 
-import { Container, Row, Col } from 'react-bootstrap'
-import Contact from '../components/complain/Contact'
+import { Container, Row, Col } from "react-bootstrap";
+import Contact from "../components/complain/Contact";
+// import chat component
+import Chat from "../components/complain/Chat";
 
-// import here
+// import user context
+import { UserContext } from "../context/userContext";
 
-// import socket.io-client 
-import {io} from 'socket.io-client'
+// import socket.io-client
+import { io } from "socket.io-client";
 
 // initial variable outside socket
-let socket
+let socket;
 export default function Complain() {
-    const [contact, setContact] = useState(null)
-    const [contacts, setContacts] = useState([])
-    // code here
+  const [contact, setContact] = useState(null);
+  const [contacts, setContacts] = useState([]);
+  // create messages state
+  const [messages, setMessages] = useState([]);
 
-    const title = "Complain admin"
-    document.title = 'DumbMerch | ' + title
+  const title = "Complain admin";
+  document.title = "DumbMerch | " + title;
 
-    // code here
+  // consume user context
+  const [state] = useContext(UserContext);
 
-    useEffect(() =>{
-        socket = io('http://localhost:5000', {
-            auth: {
-                token: localStorage.getItem("token")
-            },
-            // code here
-        })
+  useEffect(() => {
+    socket = io("http://localhost:5000", {
+      auth: {
+        token: localStorage.getItem("token"),
+      },
+      query: {
+        id: state.user.id,
+      },
+    });
 
-        // code here
-        
-        // listen error sent from server
-        socket.on("connect_error", (err) => {
-            console.error(err.message); // not authorized
-          });
-        loadContact()
-        // code here
+    // define corresponding socket listener
+    socket.on("new message", () => {
+      console.log("contact", contact);
+      console.log("triggered", contact?.id);
+      socket.emit("load messages", contact?.id);
+    });
 
-        return () => {
-            socket.disconnect()
-        }
-    }, []) // code here
+    // listen error sent from server
+    socket.on("connect_error", (err) => {
+      console.error(err.message); // not authorized
+    });
+    loadContact();
+    loadMessages();
 
-    const loadContact = () => {
-        // emit event load admin contact
-        socket.emit("load admin contact")
-        // listen event to get admin contact
-        socket.on("admin contact", (data) => {
-            // manipulate data to add message property with the newest message
-            // code here
-        })
+    return () => {
+      socket.disconnect();
+    };
+  }, [messages]);
+
+  const loadContact = () => {
+    // emit event load admin contact
+    socket.emit("load admin contact");
+    // listen event to get admin contact
+    socket.on("admin contact", async (data) => {
+      // manipulate data to add message property with the newest message
+      const dataContact = {
+        ...data,
+        message: messages.length > 0 ? messages[messages.length - 1].message : "Click here to start message",
+      };
+      setContacts([dataContact]);
+    });
+  };
+
+  // used for active style when click contact
+  const onClickContact = (data) => {
+    setContact(data);
+    // emit event load messages
+    socket.emit("load messages", data.id);
+  };
+
+  const loadMessages = (value) => {
+    // define listener on event "messages"
+    socket.on("messages", async (data) => {
+      // get data messages
+      if (data.length > 0) {
+        const dataMessages = data.map((item) => ({
+          idSender: item.sender.id,
+          message: item.message,
+        }));
+        console.log(dataMessages);
+        setMessages(dataMessages);
+      }
+      const chatMessagesElm = document.getElementById("chat-messages");
+      chatMessagesElm.scrollTop = chatMessagesElm?.scrollHeight;
+    });
+  };
+  const onSendMessage = (e) => {
+    // listen only enter key event press
+    if (e.key === "Enter") {
+      const data = {
+        idRecipient: contact.id,
+        message: e.target.value,
+      };
+
+      //emit event send message
+      socket.emit("send message", data);
+      e.target.value = "";
     }
+  };
 
-    // used for active style when click contact
-    const onClickContact = (data) => {
-        setContact(data)
-        // code here
-    }
-
-    // code here
-
-    return (
-        <>
-            <Navbar title={title} />
-            <Container fluid style={{height: '89.5vh'}}>
-                <Row>
-                    <Col md={3} style={{height: '89.5vh'}} className="px-3 border-end border-dark overflow-auto">
-                        <Contact dataContact={contacts}  clickContact={onClickContact} contact={contact} />
-                    </Col>
-                    {/* code here */}
-                </Row>
-            </Container>
-        </>
-    )
+  return (
+    <>
+      <Navbar title={title} />
+      <Container fluid style={{ height: "89.5vh" }}>
+        <Row>
+          <Col md={3} style={{ height: "89.5vh" }} className="px-3 border-end border-dark overflow-auto">
+            <Contact dataContact={contacts} clickContact={onClickContact} contact={contact} />
+          </Col>
+          <Col md={9} style={{ maxHeight: "89.5vh" }} className="px-0">
+            <Chat contact={contact} messages={messages} user={state.user} sendMessage={onSendMessage} />
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
 }
--- server/src/socket/index.js
+++ server/src/socket/index.js
@@ -1,24 +1,32 @@
 // import models
-const {chat, user, profile} = require("../../models")
-// import here
+const { chat, user, profile } = require("../../models");
+// import jsonwebtoken
+const jwt = require("jsonwebtoken");
 
-// init variable here
+// import sequelize operator
+// https://sequelize.org/master/manual/model-querying-basics.html#operators
+const { Op } = require("sequelize");
+
+const connectedUser = {};
 const socketIo = (io) => {
-
   // create middlewares before connection event
   // to prevent client access socket server without token
   io.use((socket, next) => {
-    if (socket.handshake.auth && socket.handshake.auth.token ) {
+    if (socket.handshake.auth && socket.handshake.auth.token) {
       next();
     } else {
       next(new Error("Not Authorized"));
     }
   });
 
-  io.on('connection', async (socket) => {
-    console.log('client connect: ', socket.id)
-    
-    // code here
+  io.on("connection", async (socket) => {
+    console.log("client connect: ", socket.id);
+
+    // get user connected id
+    const userId = socket.handshake.query.id;
+
+    // save to connectedUser
+    connectedUser[userId] = socket.id;
 
     // define listener on event load admin contact
     socket.on("load admin contact", async () => {
@@ -34,18 +42,18 @@ const socketIo = (io) => {
             },
           ],
           where: {
-            status: "admin"
+            status: "admin",
           },
           attributes: {
             exclude: ["createdAt", "updatedAt", "password"],
           },
         });
-    
-        socket.emit("admin contact", adminContact)
+
+        socket.emit("admin contact", adminContact);
       } catch (err) {
-        console.log(err)
+        console.log(err);
       }
-    })
+    });
 
     // define listener on event load customer contact
     socket.on("load customer contacts", async () => {
@@ -77,32 +85,100 @@ const socketIo = (io) => {
           attributes: {
             exclude: ["createdAt", "updatedAt", "password"],
           },
-        })
+        });
 
-        customerContacts = JSON.parse(JSON.stringify(customerContacts))
+        customerContacts = JSON.parse(JSON.stringify(customerContacts));
         customerContacts = customerContacts.map((item) => ({
           ...item,
           profile: {
             ...item.profile,
-            image: item.profile?.image
-              ? process.env.PATH_FILE + item.profile?.image
-              : null,
+            image: item.profile?.image ? process.env.PATH_FILE + item.profile?.image : null,
           },
-        }))
-        
-        socket.emit("customer contacts", customerContacts)
-      } catch (err) {
-        console.log(err)
-      }
-    })
+        }));
 
-    // code here
+        socket.emit("customer contacts", customerContacts);
+      } catch (err) {
+        console.log(err);
+      }
+    });
+
+    // define listener on event load messages
+    socket.on("load messages", async (payload) => {
+      try {
+        const token = socket.handshake.auth.token;
+
+        const tokenKey = process.env.TOKEN_KEY;
+        const verified = jwt.verify(token, tokenKey);
+
+        const idRecipient = payload; // catch recipient id sent from client
+        const idSender = verified.id; //id user
+
+        const data = await chat.findAll({
+          where: {
+            idSender: {
+              [Op.or]: [idRecipient, idSender],
+            },
+            idRecipient: {
+              [Op.or]: [idRecipient, idSender],
+            },
+          },
+          include: [
+            {
+              model: user,
+              as: "recipient",
+              attributes: {
+                exclude: ["createdAt", "updatedAt", "password"],
+              },
+            },
+            {
+              model: user,
+              as: "sender",
+              attributes: {
+                exclude: ["createdAt", "updatedAt", "password"],
+              },
+            },
+          ],
+          order: [["createdAt", "ASC"]],
+          attributes: {
+            exclude: ["createdAt", "updatedAt", "idRecipient", "idSender"],
+          },
+        });
+
+        socket.emit("messages", data);
+      } catch (error) {
+        console.log(error);
+      }
+    });
+
+    // define listener on event send message
+    socket.on("send message", async (payload) => {
+      try {
+        const token = socket.handshake.auth.token;
+
+        const tokenKey = process.env.TOKEN_KEY;
+        const verified = jwt.verify(token, tokenKey);
+
+        const idSender = verified.id; //id user
+        const { message, idRecipient } = payload; // catch recipient id and message sent from client
+
+        await chat.create({
+          message,
+          idRecipient,
+          idSender,
+        });
+
+        // emit to just sender and recipient default rooms by their socket id
+        io.to(socket.id).to(connectedUser[idRecipient]).emit("new message", idRecipient);
+      } catch (error) {
+        console.log(error);
+      }
+    });
 
     socket.on("disconnect", () => {
-      console.log("client disconnected", socket.id)
-      // code here
-    })
-  })
-}
+      console.log("client disconnected", socket.id);
+      delete connectedUser[userId];
+    });
+  });
+};
 
-module.exports = socketIo
+module.exports = socketIo;
