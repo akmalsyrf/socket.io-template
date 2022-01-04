0811206d7b21a27fc11c648d6d039c86ec0da1d8
akmalsyrf <dev.akmal69@gmail.com>
2022-01-04 09:53:23 +0700

    update

--- client/src/pages/Complain.js
+++ client/src/pages/Complain.js
@@ -1,21 +1,30 @@
 // import hook
-import React from 'react'
+import React, { useEffect } from "react";
 
-import Navbar from '../components/Navbar'
+import Navbar from "../components/Navbar";
 
 // import package here
+import { io } from "socket.io-client";
 
 // init variable here
+let socket;
+
 export default function Complain() {
+  const title = "Complain";
+  document.title = "DumbMerch | " + title;
 
-    const title = "Complain"
-    document.title = 'DumbMerch | ' + title
+  // code here
+  useEffect(() => {
+    socket = io("http://localhost:5000");
 
-    // code here
-    
-    return (
-        <>
-            <Navbar title={title} />
-        </>
-    )
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
+  return (
+    <>
+      <Navbar title={title} />
+    </>
+  );
 }
